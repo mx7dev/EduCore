e742c4e2a9afedc4a4bf84132cdb93f238b2f183
Maximo <mcarrion@lagramaperu.com>
2026-07-19 04:31:20 -0500

    feat: setup Angular 22 with PrimeNG 22 Aura theme and environment config

--- frontend/educore-front/src/app/app.config.ts
+++ frontend/educore-front/src/app/app.config.ts
@@ -1,11 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.dark-mode'
+        }
+      },
+      license: environment.primeNgLicenseKey
+    })
   ]
 };
---
+++ frontend/educore-front/src/environments/environment.example.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  primeNgLicenseKey: 'YOUR_LICENSE_KEY_HERE'
+};
